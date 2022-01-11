7ced5ebaf9e00be1bc1769033ed230a389123355
Filip Vyrostko <2479054v@student.gla.ac.uk>
2022-01-11 13:22:26 +0000

    Added register/login/logout views with full functionality. Only remaining part is to include forms for the fist two functions.

--- leidos_project/leidos_app/views.py
+++ leidos_project/leidos_app/views.py
@@ -6,9 +6,89 @@ from django.http import HttpResponse
 def base(request):
     return render(request, 'leidos_app/base.html')
 
-def login(request):
-    return render(request, 'leidos_app/login.html')
-
 def register(request):
-    return render(request, 'leidos_app/register.html')
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            # Set profile image if included (else set default [set in models])
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+
+
+
+            profile.save()
+            registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'leidos_app/register.html', context={'user_form': user_form,
+                                                                     'profile_form': profile_form,
+                                                                     'registered': registered})
+
+
+def user_login(request):
+    # If the request is an HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        # We use request.POST.get('<variable>') as opposed
+        # to request.POST['<variable>'], because the
+        # request.POST.get('<variable>') returns None if the
+        # value does not exist, while request.POST['<variable>']
+        # will raise a KeyError exception.
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return redirect(reverse('leidos_app:index'))
+            else:
+                # An inactive account was used - no logging in!
+                messages.error(request, "Your Leidos account is disabled")
+                return redirect(reverse("leidos_app:login"))
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print(f"Invalid login details: {username}, {password}")
+            messages.error(request, "Invalid login details")
+            return redirect(reverse("leidos_app:login"))
+    # The request is not a HTTP POST, so display the login form
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'leidos_app/login.html')
+
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('leidos_app:index'))
+
+
     
