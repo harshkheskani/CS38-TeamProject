78028a4ba7cc0082148bdda6dcdc731a0809f650
Filip Vyrostko <2479054v@student.gla.ac.uk>
2022-01-13 10:46:34 +0000

    Included forms.py and modified views.py and template files
    Included UserProfile form and User form to get data from user for login and register. Modified views.py by adding multiple imports to enable necesarry functionality. Changed register.html to display correct input fields.

--- leidos_project/leidos_app/views.py
+++ leidos_project/leidos_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from leidos_app.forms import *
+from leidos_project.leidos_app.forms import *
 from django.contrib import messages
 from django.urls import reverse
 
