557ad2dbfc1da7b7902038a5150f3ddf79df285f
Alen Reji <2460290r@student.gla.ac.uk>
2022-02-10 21:47:09 +0000

    extended working population script

--- leidos_project/populate.py
+++ leidos_project/populate.py
@@ -10,14 +10,25 @@ from leidos_app.models import UserProfile, Business, MenuSection, SectionItem
 def populate():
     users = [{'username': 'ben', 'firstname': 'ben',
               'lastname': 'bennison', 'password': 'test', 'profile_pic': 'media/profile_images/default.png', 
-              'description': 'ahahahaaha', 'is_business_owner': 'True'}]
+              'description': 'ahahahaaha', 'is_business_owner': 'True'},
+              {'username': 'james', 'firstname': 'james',
+              'lastname': 'jameson', 'password': 'test', 'profile_pic': 'media/profile_images/default.png', 
+              'description': 'ayo', 'is_business_owner': 'True'},
+              {'username': 'clark', 'firstname': 'clark',
+              'lastname': 'clarkson', 'password': 'test', 'profile_pic': 'media/profile_images/default.png', 
+              'description': 'i dont have a business', 'is_business_owner': 'False'}]
 
-    businesses = [{'owner': 'ben', 'name':'Bobs Burgers', 'address': 'University Avenue, G12 8QW', 'img': 'media/business_images/business_default.png', 
-              }]
+    businesses = [{'owner': 'ben', 'name':'Bobs Burgers', 'address': 'University Avenue, G12 8QW', 
+                    'img': 'media/business_images/business_default.png'},
+                    {'owner': 'james', 'name':'James Burgers', 'address': '132 University Pl, Glasgow G12 8TA', 
+                    'img': 'media/business_images/business_default.png'}]
 
     menuSections = [{'business': 'Bobs Burgers', 'name': 'Mains'},
                     {'business': 'Bobs Burgers', 'name': 'Sides'},
-                    {'business': 'Bobs Burgers', 'name': 'Drinks'}]
+                    {'business': 'Bobs Burgers', 'name': 'Drinks'},
+                    {'business': 'James Burgers', 'name': 'Burgers'},
+                    {'business': 'James Burgers', 'name': 'Cakes'},
+                    {'business': 'James Burgers', 'name': 'Drinks'}]
 
     sectionItems = [{'business': 'Bobs Burgers', 'section': 'Mains', 'name': 'Cheeseburger', 
                         'description': 'food', 'price': '5', 'img': 'media/item_images/defaultfood.png'},
@@ -28,7 +39,17 @@ def populate():
                     {'business': 'Bobs Burgers', 'section': 'Drinks', 'name': 'Fanta', 
                         'description': 'food', 'price': '2', 'img': 'media/item_images/defaultfood.png'},
                     {'business': 'Bobs Burgers', 'section': 'Sides', 'name': 'Fries', 
-                        'description': 'food', 'price': '3', 'img': 'media/item_images/defaultfood.png'}]
+                        'description': 'food', 'price': '3', 'img': 'media/item_images/defaultfood.png'},
+                    {'business': 'James Burgers', 'section': 'Burgers', 'name': 'Cheeseburger', 
+                        'description': 'food', 'price': '3', 'img': 'media/item_images/defaultfood.png'},
+                    {'business': 'James Burgers', 'section': 'Burgers', 'name': 'Chicken burger', 
+                        'description': 'food', 'price': '4', 'img': 'media/item_images/defaultfood.png'},
+                    {'business': 'James Burgers', 'section': 'Cakes', 'name': 'Sponge Cake', 
+                        'description': 'food', 'price': '2', 'img': 'media/item_images/defaultfood.png'},
+                    {'business': 'James Burgers', 'section': 'Cakes', 'name': 'Chocolate Cake', 
+                        'description': 'food', 'price': '3', 'img': 'media/item_images/defaultfood.png'},
+                    {'business': 'James Burgers', 'section': 'Drinks', 'name': 'Coke', 
+                        'description': 'food', 'price': '2', 'img': 'media/item_images/defaultfood.png'}]
 
     for u in users:
         add_user(u['username'], u['password'])
@@ -64,7 +85,7 @@ def add_menuSection(business, name):
     new_section.save()
 
 def add_sectionItem(business, section, name, description, price, img):
-    new_item = SectionItem(section_fk=MenuSection.objects.get(name=section))
+    new_item = SectionItem(section_fk=MenuSection.objects.filter(business_fk=Business.objects.get(name=business)).get(name=section))
     new_item.name = name
     new_item.description = description
     new_item.price = price
