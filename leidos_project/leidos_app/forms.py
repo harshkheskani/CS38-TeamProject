a0cfe625095a9606f012be732945fac2c680acfa
Filip Vyrostko <2479054v@student.gla.ac.uk>
2022-02-13 18:15:01 +0000

    Extended forms with widgets
    Extended forms with widgets to improve visual representation. More changes need to be done such as making them shorter width vise. Set class for buttons in login/register/register_business to btn-primary. Included form-control class to login page inputs
    
    Signed-off-by: Filip Vyrostko <2479054v@student.gla.ac.uk>

--- leidos_project/leidos_app/forms.py
+++ leidos_project/leidos_app/forms.py
@@ -6,19 +6,35 @@ from .models import *
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'password')
 
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "form-control"}),
+            "password": forms.PasswordInput(attrs={"class": "form-control"}),
+        }
+
 
 class UserProfileForm(forms.ModelForm):
-    is_business_owner = forms.ChoiceField(choices=[("Yes","Yes"), ("No","No")])
+    is_business_owner = forms.ChoiceField(choices=[("Yes","Yes"), ("No","No")],
+                                          label="Are you a business owner?",
+                                          widget=forms.Select(attrs={"class": "form-control"}))
 
     class Meta:
         model = UserProfile
         fields = ('profile_pic',)
+
+        labels = {
+            "profile_pic": "Profile Picture",
+        }
+
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "form-control"}),
+            "password": forms.PasswordInput(attrs={"class": "form-control"}),
+            "profile_pic": forms.FileInput(attrs={"class": "form-control"}),
+        }
 
 
 class AddSectionForm(forms.ModelForm):
@@ -79,7 +95,14 @@ class RegisterBusinessForm(forms.ModelForm):
         exclude = ('owner_fk',)
 
         labels = {
-            "img": "Business Cover Image"
+            "img": "Cover Image",
+        }
+
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "address": forms.TextInput(attrs={"class": "form-control"}),
+            "img": forms.ClearableFileInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control"}),
         }
 
 class EditBusinessForm(forms.ModelForm):
@@ -88,8 +111,25 @@ class EditBusinessForm(forms.ModelForm):
         model = Business
         exclude = ('owner_fk', 'slug', 'name')
 
+        labels = {
+            "img": "Cover Image"
+        }
+
+        widgets = {
+            "address": forms.TextInput(attrs={"class": "form-control"}),
+            "img": forms.ClearableFileInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control"}),
+        }
+
 class EditOpeningHours(forms.ModelForm):
 
     class Meta:
         model = OpeningHours
         exclude = ('business_fk',)
+
+        widgets = {
+            "weekday_from": forms.Select(attrs={"class": "form-control"}),
+            "weekday_to": forms.Select(attrs={"class": "form-control"}),
+            "from_hour": forms.Select(attrs={"class": "form-control"}),
+            "to_hour": forms.Select(attrs={"class": "form-control"}),
+        }
