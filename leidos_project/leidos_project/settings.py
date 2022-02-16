3cf35d9deaeac79d0dada662a235d549dc480996
Alen Reji <2460290r@student.gla.ac.uk>
2022-02-16 12:08:27 +0000

    initial deploy

--- leidos_project/leidos_project/settings.py
+++ leidos_project/leidos_project/settings.py
@@ -151,13 +151,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
